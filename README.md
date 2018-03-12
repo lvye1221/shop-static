e7dc50d48bc9a83edf73a494d99166114846bd52
jack <616651001@qq.com>
2018-03-13 01:52:45 +0800

    添加后台接口代码及文档

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # shop-static
 电商项目静态页面
+
+# api 启动方法
+
+
+1. 启动 MongoDB
+
+第一次启动时，需要导入数据库，shop
+
+```
+打开 MongoBooster 可视化界面工具
+
+
+创建数据库 shop
+
+导入 goods.json 和 users.json 数据
+
+```
+
+打开命令窗口
+```
+mongo --dbpath=c:\mongo
+```
+
+2. 启动服务器
+
+在当前目录再 打开命令窗口
+
+第一次启动时，需要安装依赖
+```
+cnpm install
+```
+
+
+```
+node bin\www
+```
+
+
---
+++ api/db.js
@@ -0,0 +1,18 @@
+let mongoose = require('mongoose');
+//连接MongoDB数据库,本地默认端口27017，数据库叫dumall
+// mongoose.connect('mongodb://127.0.0.1:27017/xshop');
+mongoose.connect('mongodb://127.0.0.1:27017/shop');
+
+mongoose.connection.on("connected", function () {//连接成功事件
+  console.log("MongoDB 数据库连接 成功.")
+});
+
+mongoose.connection.on("error", function () {//连接失败事件
+  console.log("MongoDB 数据库连接 失败. 请启动MongoDB服务器： mongod --dbpath=c:\mongo")
+});
+
+mongoose.connection.on("disconnected", function () {//断开连接事件
+  console.log("MongoDB 数据库连接 端开.")
+});
+
+module.exports = mongoose;
