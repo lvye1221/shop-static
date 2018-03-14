b64fe31a8d9fe4e1841ea0e1f20829d20ec051e5
杨杰 <616651001@qq.com>
2018-03-14 19:14:59 +0800

    修改了跨域的问题

--- api/app.js
+++ api/app.js
@@ -19,6 +19,7 @@ app.use(function (req,res,next) {
 
 	// 只
     // res.header("Access-Control-Allow-Origin", "http://127.0.0.1:3000");
+    // res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Origin", "http://hope1995.me:3000");
     res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
